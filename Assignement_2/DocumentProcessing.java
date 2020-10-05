c4f0e02089e16c4a6e447ac958218dd6d7341be4
PaulCalot <paul.calot@hotmail.fr>
2020-10-05 19:39:52 +0200

    Commit before splitting pattern

--- Assignement_2/DocumentProcessing.java
+++ Assignement_2/DocumentProcessing.java
@@ -26,9 +26,10 @@ public class DocumentProcessing {
   public static void parseBuffer(CharSequence data) {
 
     int start = 0;
-
+    String line_terminators = "\\n|\\r\\n|\\r|\\u0085|\\u2028|\\u2029";
+    
     //Pattern p = Pattern.compile("<A((.|\\n)*?)href(\\s*?)=(\\s*?)\"(.*?)\"(.*?)>|<A((.|\\n)*?)href(\\s*?)=(\\s*?)\\'(.*?)\\'(.*?)>", Pattern.CASE_INSENSITIVE); // reluctant quantifiers
-    Pattern p = Pattern.compile("<A((.|\\n)*?)href(\\s*?)=(\\s*?)(\"(?=([^']*?)\")|'(?=([^\"]*?)'))(.*?)(\"|')(.*?)>", Pattern.CASE_INSENSITIVE); // reluctant quantifiers
+    Pattern p = Pattern.compile("<A((\\s((.|" + line_terminators + ")*?)\\s)|\\s)href(\\s*?)=(\\s*?)(\"(?=([^']*?)\")|'(?=([^\"]*?)'))(.*?)(\"|')((.|" + line_terminators + ")*?)>", Pattern.CASE_INSENSITIVE); // reluctant quantifiers
 
     Matcher m = p.matcher(data);
     
@@ -37,8 +38,8 @@ public class DocumentProcessing {
       String possible_match = m.group();
       
       // Debug
-      //System.out.println(" ");
-      //System.out.println("Possible match in position " + Integer.toString(start) + " : " + possible_match);
+      System.out.println(" ");
+      System.out.println("Possible match in position " + Integer.toString(start) + " : " + possible_match);
 
       Pattern p_ = Pattern.compile("href(\\s*?)=(\\s*?)(\"(?=([^']*?)\")|'(?=([^\"]*?)'))(.*?)(\"|')", Pattern.CASE_INSENSITIVE);
       Matcher m_ = p_.matcher(possible_match);
@@ -46,7 +47,7 @@ public class DocumentProcessing {
         // and that there is no need for other securities on the presence of certain patterns.
         String possible_url = m_.group();
         // debug
-        //System.out.println("Possible url in position : " + possible_url);
+        System.out.println("Possible url in position : " + possible_url);
 
         possible_url = possible_url.split("=(\\s)*?(\"(?=([^']*?)\")|'(?=([^\"]*?)'))", 2)[1];
         possible_url = possible_url.substring(0,possible_url.length()-1);
@@ -67,12 +68,11 @@ public class DocumentProcessing {
       }
   }
 
-  /*public static void main(String[] args) {
-    String data = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"><html><head><title>test page 1</title></head><body> <hr><A aoeihaod \n href=\"http://www.enseignement.polytechnique.fre/Philippe.Chassignet/test/p2.html\">page 2</A>--<a target=\"blank_\" HREF  = \"http://www.enseignement.polytechnique.fr/profs/informatique/Philippe.Chassignet/test/p4.html' /><a href = \"http://www.enseignement.polytechnique.fr/profs/informatique/Philippe.Chassignet/test/p1.html\" id='1'>page 1 again</a><A href=\"https://www.enseignement.polytechnique.fr/profs/informatique/Philippe.Chassignet/test/p5.html\">page 5</A><hr></body></html>";
-
+  public static void main(String[] args) {
+    //String data = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"><html><head><title>test page 1</title></head><body> <hr><A aoeihaod \n href=\"http://www.enseignement.polytechnique.fre/Philippe.Chassignet/test/p2.html\">page 2</A>--<a target=\"blank_\" HREF  = \"http://www.enseignement.polytechnique.fr/profs/informatique/Philippe.Chassignet/test/p4.html' /><a href = \"http://www.enseignement.polytechnique.fr/profs/informatique/Philippe.Chassignet/test/p1.html\" id='1'>page 1 again</a><A href=\"https://www.enseignement.polytechnique.fr/profs/informatique/Philippe.Chassignet/test/p5.html\">page 5</A><hr></body></html>";
+    String data = "<a href='http://host/file.ext1'><a href='http://host/file.ext2'>blah</a>";
     DocumentProcessing.parseBuffer(data);
-
-  }*/
+  }
 }
 
 
