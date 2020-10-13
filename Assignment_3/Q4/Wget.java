4a5e21f618327d6362dbc979265d86f49439a13c
PaulCalot <paul.calot@hotmail.fr>
2020-10-13 11:11:37 +0200

    Added Wget correction for NullPointerException + added corrections for TimeOut

--- Assignment_3/Q4/Wget.java
+++ Assignment_3/Q4/Wget.java
@@ -1,7 +1,6 @@
 // Paul Calot && Philippe SAGBO
 import java.util.HashSet;
 import java.util.NoSuchElementException;
-
 import java.lang.InterruptedException;
 import java.io.FileReader;
 import java.io.BufferedReader;
@@ -9,8 +8,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Wget {
-  static final boolean debug = false;
 
+  @SuppressWarnings("unused")
   public static void iterativeDownload(String initialURL) {
     final URLQueue queue = new ListQueue();
     final HashSet<String> seen = new HashSet<String>();
@@ -20,35 +19,35 @@ public class Wget {
       @Override
       public void takeUrl(String url) {
         // to be completed at exercise 2
-	if(seen.add(url)){
-		queue.enqueue(url);
-	
-		MyURL my_url = new MyURL(url);
-		Xurl.download(url);
-		String filename;
-		if(my_url.getPath().equals("/"))
-			filename = "index";
-		else{
-			String[] names = my_url.getPath().split("/");
-			filename = names[names.length - 1];
-		}
-		String data = "";
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(filename));
-			String line=reader.readLine();
-			while(line!=null){
-				data = data.concat(line);
-				line = reader.readLine();
-			}
-			reader.close();
-		} 
-		catch(IOException e){
-			System.err.println(e.getMessage());
-		}
+        if(seen.add(url)){
+                queue.enqueue(url);
+        
+                MyURL my_url = new MyURL(url);
+                Xurl.download(url);
+                String filename;
+                if(my_url.getPath().equals("/"))
+                        filename = "index";
+                else{
+                        String[] names = my_url.getPath().split("/");
+                        filename = names[names.length - 1];
+                }
+                String data = "";
+                try {
+                        BufferedReader reader = new BufferedReader(new FileReader(filename));
+                        String line=reader.readLine();
+                        while(line!=null){
+                                data = data.concat(line);
+                                line = reader.readLine();
+                        }
+                        reader.close();
+                } 
+                catch(IOException e){
+                        System.err.println(e.getMessage());
+                }
 
-		DocumentProcessing.parseBuffer(data);
-		queue.dequeue();
-	}
+                DocumentProcessing.parseBuffer(data);
+                queue.dequeue();
+        }
       }
     };
     // to start, we push the initial url into the queue
@@ -60,31 +59,30 @@ public class Wget {
    
 }
 
-  @SuppressWarnings("unused")
   public static void multiThreadedDownload(String initialURL) {
     final long initialThreadNumber = Thread.activeCount();
     final SynchronizedListQueue queue = new SynchronizedListQueue();
-    final MyHashSet<String> seen = new MyHashSet<String>();
-    final long delay = 1000;
+    final MyHashSet seen = new MyHashSet();
+    final long delay = 0;
+    final boolean debug = false;
     // defines a new URLhandler
     DocumentProcessing.handler = new DocumentProcessing.URLhandler() {
       // this method will be called for each matched url
       @Override
       public void takeUrl(String url) {
-        // to be completed at exercise 2
         if(seen.add(url)){
           queue.enqueue(url);
         }
       }
     };
-    queue.enqueue(initialURL);
-    seen.add(initialURL);
+    DocumentProcessing.handler.takeUrl(initialURL);
     do{
       try {
-        Thread t = new Thread(new Producer(queue.dequeue()));
+        String uurl=queue.dequeue();
+        Thread t = new Thread(new Producer(uurl, seen), uurl);
         t.start();
       }
-    catch(NoSuchElementException e1){
+      catch(NoSuchElementException e1){
         if(debug)System.err.println(e1.getMessage());
         try {
           Thread.sleep(delay);
@@ -92,94 +90,23 @@ public class Wget {
         catch(InterruptedException e2){
           if(debug)System.err.println(e2.getMessage());
         }
-    }
-    }while((Thread.activeCount()>initialThreadNumber) || !queue.isEmpty());
+        if((Thread.activeCount()==initialThreadNumber) && queue.isEmpty()) break;
+      }
+    }while(true);
   }
 
 
   @SuppressWarnings("unused")
   public static void threadPoolDownload(int poolSize, String initialURL) {
-    final long initialThreadNumber = Thread.activeCount(); //+Integer.toLong(poolSize);
-    final BlockingListQueue queue = new BlockingListQueue();
-    final MyHashSet<String> seen = new MyHashSet<>();
-    final long delay = 1000;
-    Thread[] threadsList = new Thread[poolSize];
-    // defines a new URLhandler
-    DocumentProcessing.handler = new DocumentProcessing.URLhandler() {
-      // this method will be called for each matched url
-      @Override
-      public void takeUrl(String url) {
-        // to be completed at exercise 2
-        if(seen.add(url)){
-          queue.enqueue(url);
-        }
-      }
-    };
-    if(debug){
-      System.out.println("Adding initial URL");
-    }
-    queue.enqueue(initialURL);
-    seen.add(initialURL);
-    for(int k=0;k<poolSize;k++){
-      threadsList[k] = new Thread(new ProducerPool(queue));
-      threadsList[k].start();
-    }
-    boolean b = true;
-    while(b){
-      // is there a way to make sure that all this method is run at once and not interrupted by other threads 
-      // (along with queue.isEmpty())
-      if((getWaitingThreadsNb(threadsList)==poolSize) && queue.isEmpty()){
-        try{
-          Thread.currentThread().sleep(delay);
-          if(debug){
-            System.out.println("Main thread is sleeping.");
-          }
-        }
-        
-        catch(InterruptedException e2){
-          if(debug)System.err.println(e2.getMessage());
-          //Thread.currentThread().interrupt();
-        }
-        // TODO : make sure we have the right terminaison condition
-        if((getWaitingThreadsNb(threadsList)==poolSize) && queue.isEmpty()){
-          if(debug){
-            System.out.println("Interrupting all threads...");
-          }
-          for(int k=0;k<poolSize;k++){
-            threadsList[k].interrupt();
-            try{
-              threadsList[k].join();
-            }catch (InterruptedException e1){}
-          }
-          b = false;
-        }
-      }
-    }
+    // to be completed later
   }
 
-  public static int getWaitingThreadsNb(Thread[] threadsList){
-    int waiting_nb = 0;
-    for(int k=0; k<threadsList.length;k++){
-        if(debug){
-          //System.out.println(Thread.currentThread().toString()+" is "+threadsList[k].getState());
-        }
-        if(threadsList[k].getState()==Thread.State.WAITING){
-          /*if(debug){
-            System.out.println(Thread.currentThread().toString()+" is waiting.");
-          }*/
-          waiting_nb+=1;
-        }
-    }
-    return waiting_nb;
-  }
   public static void main(String[] args) {
     if (args.length < 1) {
       System.err.println("Usage: java Wget url");
       System.exit(-1);
     }
-    //iterativeDownload(args[0]);
-    //multiThreadedDownload(args[0]);
-    threadPoolDownload(Integer.parseInt(args[0]), args[1]);
+    multiThreadedDownload(args[0]);
   }
 
 }
@@ -188,100 +115,50 @@ public class Wget {
 class Producer implements Runnable {
   // private SynchronizedListQueue m_queue; // takeURL alrady has it
   private String m_url;
+  private MyHashSet s;
   private boolean debug = true;
-  public Producer(String url){ //SynchronizedListQueue queue, 
-    //m_queue = queue;
+  public Producer(String url, MyHashSet seen){ 
     m_url = url;
+    s = seen;
   }
 
   public void run(){
     if(debug){
-      System.out.println(Thread.currentThread() + " was here.");
+        System.out.println(Thread.currentThread() + " was here [+].");
     }
     Xurl.download(this.m_url);
     String filename;
     MyURL my_url = new MyURL(this.m_url);
-		if(my_url.getPath().equals("/"))
-			filename = "index";
-		else{
-			String[] names = my_url.getPath().split("/");
-			filename = names[names.length - 1];
-    }
-    // TODO ? Make sure we are not replacing an existing file
- 		String data = "";
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(filename)); 
-			String line=reader.readLine();
-			while(line!=null){
-				data = data.concat(line);
-				line = reader.readLine();
-			}
-			reader.close();
-		} 
-		catch(IOException e){
-			System.err.println(e.getMessage());
-    }
-
-		DocumentProcessing.parseBuffer(data); // seek new urls and call takeURL on them
-  }
-}
-
-
-class ProducerPool implements Runnable {
-  private BlockingListQueue m_queue; // takeURL alrady has it
-  private boolean debug = false;
-
-  public ProducerPool(BlockingListQueue queue){
-    m_queue = queue;
-  }
-
-  public void run(){
-    while(!Thread.currentThread().isInterrupted()){
-      if(debug){  
-        System.out.println(Thread.currentThread() + " was here.");
-      }
-      
-      String url = m_queue.dequeue(); // the thread may be force to wait in the given dequeue function
-      /*if(url.equals("**STOP**")){ 
-        Thread.currentThread().interrupt();
-        break;
-      }*/
-      
-      if(Thread.currentThread().isInterrupted()){
-        Thread.currentThread().interrupt();
-        break;
-      }
-
-      Xurl.download(url); 
-      String filename;
-      MyURL my_url = new MyURL(url);
-      if(my_url.getPath().equals("/"))
+    if(my_url.getPath().equals("/"))
         filename = "index";
-      else{
+    else{
         String[] names = my_url.getPath().split("/");
         filename = names[names.length - 1];
-      }
-      String data = "";
-      try {
+    }
+    // TODO ? Make sure we are not replacing an existing file
+    String data = "";
+    try {
         BufferedReader reader = new BufferedReader(new FileReader(filename)); 
         String line=reader.readLine();
         while(line!=null){
-          data = data.concat(line);
-          line = reader.readLine();
+            data = data.concat(line);
+            line = reader.readLine();
         }
         reader.close();
-      } 
-      catch(IOException e){
-        System.err.println(e.getMessage());
-      }
-      DocumentProcessing.parseBuffer(data); // seek new urls and call takeURL on them
+    } 
+    catch(IOException e){
+        if(debug)System.err.println(e.getMessage());
+    }
+    DocumentProcessing.parseBuffer(data); // seek new urls and call takeURL on them
+    if(debug){
+      System.out.println(Thread.currentThread() + " was here [-].");
     }
   }
 }
 
-class MyHashSet<E> extends HashSet<E> {
-  @Override public synchronized boolean add(Object o) {
-    return super.add((E) o);
+class MyHashSet extends HashSet<String> {
+  @Override public synchronized boolean add(String s) {
+    return super.add(s);
   }
 }
 
