d6628e75a83f17ed99e76acfb56b2bece24a19ee
PaulCalot <paul.calot@hotmail.fr>
2020-11-14 18:22:07 +0100

    Exercice 2 - first completion - not working commit

--- Assignment_6/ConnectedHandler.java
+++ Assignment_6/ConnectedHandler.java
@@ -1,6 +1,6 @@
 import java.util.Timer;
 import java.util.TimerTask;
-
+import java.util.Date;
 public class ConnectedHandler extends Handler {
 
   /**
@@ -33,8 +33,9 @@ public class ConnectedHandler extends Handler {
   private final int localId;
   private final String destination;
   private Handler aboveHandler;
-  // to be completed
-
+  private int remoteId;
+  private int packetNumber;
+  private Handler under;
   /**
    * Initializes a new connected handler with the specified parameters
    * 
@@ -50,7 +51,9 @@ public class ConnectedHandler extends Handler {
     super(_under, _localId, true);
     this.localId = _localId;
     this.destination = _destination;
-    // to be completed
+    this.remoteId = -1;
+    this.packetNumber = 0;
+    this.under = _under;
     send(HELLO);
   }
 
@@ -67,12 +70,90 @@ public class ConnectedHandler extends Handler {
 
   @Override
   public void handle(Message message) {
-    // to be completed
+    // message is of the form : 
+    // 1. ACK : destinationId;senderId;packetNumber;--ACK--
+    // 2. PACKET : destinationId;senderId;packetNumber;payload
+    // 3. HELLO : destinationId;-1;packetNumber;--HELLO--
+
+    String payload = message.payload;
+    String sourceAddress = message.sourceAddress;
+
+    String[] split = payload.split(";",4);
+
+    // HELLO message
+    if(this.remoteId == -1 && split[1].equals("-1") && split[2].equals(Integer.toString(this.packetNumber)) && split[3].equals(HELLO)){
+      this.remoteId = Integer.parseInt(split[0]);
+      // in this order !!
+      this.send(ACK);
+      this.packetNumber = (this.packetNumber+1)%2;
+      // no need to notify
+    }
+    else if(this.remoteId == Integer.parseInt(split[0]) && Integer.parseInt(split[0]) == this.localId){
+      // in this case, we check if it's an acknoledgement
+      int packetNumber_ = Integer.parseInt(split[2]);
+      String payload_ = split[3];
+      if(packetNumber_==this.packetNumber){
+        // then we are recieving an expected ACK most likely
+        if(payload_.equals(ACK)){
+          this.packetNumber = (this.packetNumber+1)%2;
+          notify();
+        }
+        else{
+          System.out.println("Expected ACK : ");
+          System.out.println("Recieved message : " + message.toString());
+          System.out.println("Current state : "+Integer.toString(this.packetNumber));
+        }
+      }
+    }
   }
 
+
   @Override
-  public void send(final String payload) {
-    // to be completed
+  public synchronized void send(final String payload) {    
+    String tmp_payload = "";
+    if(payload.equals(HELLO)){
+      tmp_payload = Integer.toString(this.localId)+";"+"-1;0"+payload;
+    }
+    else{
+      tmp_payload = Integer.toString(this.localId)+";"+Integer.toString(this.remoteId)+";"+Integer.toString(this.packetNumber)+";"+payload;
+    }
+    String formatted_payload = tmp_payload;
+    TimerTask task;
+    Handler handler_ = this.under;
+    String destination_ = this.destination;
+    if(payload.equals(ACK)){
+      // send acklodegment only once
+      task = new TimerTask(){
+        @Override
+        public void run() {
+          handler_.send(formatted_payload, destination_);          
+        }
+      };
+      TIMER.schedule(task, new Date());
+    }
+    else{    // waiting for acknoledgement
+      task = new TimerTask(){
+        int count = 0;
+        @Override
+        public void run() {
+          count +=1;
+          handler_.send(formatted_payload, destination_);          
+          if(count>MAX_REPEAT){
+            this.cancel();
+            notify(); // to break WAITING mode of send
+          }
+        }
+      };
+      TIMER.schedule(task, new Date(), DELAY);
+    }
+    try {
+      wait();
+    }
+    catch(InterruptedException e){
+      System.err.println(e.getMessage());
+    }
+    task.cancel();
+    TIMER.purge();
   }
 
   @Override
@@ -82,8 +163,9 @@ public class ConnectedHandler extends Handler {
 
   @Override
   public void close() {
-    // to be completed
+    // TO BE COMPLETED
+    TIMER.cancel();
+    System.err.println("ConnectedHandler closed");
     super.close();
   }
-
 }
--- Assignment_6/t.sh
+++ Assignment_6/t.sh
@@ -1,5 +1,5 @@
 rm *.class
 sed -i 's/= false/= true/' GroundLayer.java
-javac Talk_1.java
-java Talk_1 9999 127.0.0.1:8999
+javac Talk_2.java
+java Talk_2 9999 127.0.0.1:8999
 sed -i 's/= true/= false/' GroundLayer.java
